b87e189a78546a96360e71ea10fe1792df27de5b
Jozehjr2 <junior_zvjr@hotmail.com>
2021-08-31 21:06:03 -0300

    created data tasks

---
+++ src/data/Tasks.jsx
@@ -0,0 +1,45 @@
+/** Tarefas usadas na aplicação
+ * Obs.: Dados para teste
+ */
+
+export default [
+  { id: 1, idUser: 2, nameTask: 'Definir layout', status: 'concluido' },
+  {
+    id: 2,
+    idUser: 2,
+    nameTask: 'Definir o guide e mapa de cores',
+    status: 'concluido',
+  },
+  {
+    id: 3,
+    idUser: 3,
+    nameTask: 'Anunciar a estreia nas redes sociais',
+    status: 'em andamento',
+  },
+  { id: 4, idUser: 3, nameTask: 'Procurar parcerias', status: 'em andamento' },
+  {
+    id: 5,
+    idUser: 4,
+    nameTask: 'Construir banco de dados',
+    status: 'concluido',
+  },
+  {
+    id: 6,
+    idUser: 5,
+    nameTask: 'Construir infraestrutura',
+    status: 'concluido',
+  },
+  { id: 7, idUser: 5, nameTask: 'Definir cronograma', status: 'concluido' },
+  {
+    id: 8,
+    idUser: 2,
+    nameTask: 'Alterar logo da empresa',
+    status: 'em andamento',
+  },
+  {
+    id: 9,
+    idUser: 6,
+    nameTask: 'Novos produtos com os parceiros',
+    status: 'em andamento',
+  },
+];
